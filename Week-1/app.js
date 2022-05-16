65065382f96168b0c991f43789809fa49ae42405
Said Akilli <9017890@student.zadkine.nl>
2022-05-16 15:00:36 +0200

    EVENTLISTENER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Week-1/app.js
+++ Week-1/app.js
@@ -1,19 +1,38 @@
-let name  ;
-
-let country  ;
-
+let name;
+let country;
 let p = document.getElementById("output");
 
-const nameChanged = function(event) {
-    name = event.target.value;
-    changeInput();
-}
-const countryChange = function(event) {
-    country = event.target.value;
-    changeInput();
-}
+// const nameChanged = function(event) {
+//     name = event.target.value;
+//     changeInput();
+// }
+// const countryChange = function(event) {
+//     country = event.target.value;
+//     changeInput();
+// }
 
 const changeInput = function() {
     const text = `Je heet ${name} en je komt uit ${country}`; 
     p.textContent = text;
 }
+
+let nameChanged = document.querySelector("#nameChanged");
+let countryChange = document.querySelector("#nameChanged");
+
+nameChanged.addEventListener("change", function(){
+        name = event.target.value;
+        changeInput();
+})
+
+countryChange.addEventListener("change", function(){
+    country = event.target.value;
+    changeInput();
+})
+
+
+
+
+nameChanged.addEventListener("change", function(){
+    name = event.target.value;
+    changeInput();
+})
