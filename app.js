65a91aa9dd39c5a1ac5b490b4b55f699be4b91cd
Said Akilli <9017890@student.zadkine.nl>
2022-05-20 12:10:37 +0200

    OPDRACHT 2

--- app.js
+++ app.js
@@ -11,4 +11,31 @@ bird.addEventListener("mouseover", function() {
      
 });
 
+window.addEventListener("keydown", function(e){
+    console.log(e)
+    if (e.key == "ArrowRight"){
+        count += 50;
+    
+        bird.style.left =  count + "px";
+    }
+    
+    if (e.key == "ArrowLeft"){
+        count -= 50;
+    
+        bird.style.left =  count + "px";
+    }
+    
+    if (e.key == "ArrowUp"){
+        count += 50;
+    
+        bird.style.bottom =  count + "px";
+    }
+    
+    if (e.key == "ArrowDown"){
+        count += 50;
+    
+        bird.style.top =  count + "px";
+    }
+});
+
 
