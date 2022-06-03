57d3823166c1524bd67d709cc0b043e6663f0c29
Said Akilli <9017890@student.zadkine.nl>
2022-06-03 11:23:15 +0200

    yessir

--- app.js
+++ app.js
@@ -17,25 +17,45 @@ window.addEventListener("keydown", function(e){
         count += 50;
     
         bird.style.left =  count + "px";
+
+        bird.src="img/b-right.svg"
     }
     
     if (e.key == "ArrowLeft"){
         count -= 50;
     
         bird.style.left =  count + "px";
+
+        bird.src="img/b-left.svg"
+
     }
     
     if (e.key == "ArrowUp"){
         count += 50;
     
         bird.style.bottom =  count + "px";
+
+        bird.src="img/b-up.svg"
+
     }
     
     if (e.key == "ArrowDown"){
         count += 50;
     
         bird.style.top =  count + "px";
+
+        bird.src="img/b-down.svg"
+
     }
 });
 
 
+function changeImg() {
+    const image = document.getElementById('myImg');
+    if (image.src.match("img/more.jpg")) {
+        image.src = "img/less.jpg";
+    }
+    else {
+        image.src = "img/more.jpg";
+    }
+}7
